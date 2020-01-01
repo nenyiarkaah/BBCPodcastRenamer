a2daeea270b09366285343a707d907f8c9760545
nenyiarkaah <nenyi.arkaah@gmail.com>
2020-01-01 19:37:08 +0000

    refactor: Updated mocking for JSON tests

--- src/test/scala/services/SimpleJsonTest.scala
+++ src/test/scala/services/SimpleJsonTest.scala
@@ -1,21 +1,24 @@
 package services
 
+import org.scalamock.scalatest.MockFactory
 import org.scalatest.Matchers._
 import org.scalatest.{BeforeAndAfter, FlatSpec}
 
 /**
   * Created by Nenyi on 21/03/2017.
   */
-class SimpleJsonTest extends FlatSpec with BeforeAndAfter with SimpleJson with testData {
+class SimpleJsonTest extends FlatSpec with BeforeAndAfter with MockFactory
+  with SimpleJson with testData {
 
   "ConvertPodcastSettingsToJson" should "be able to convert PodcastSettings to JSON" in {
     ConvertPodcastSettingsToJson(defaultSettings) shouldBe testJSON
   }
 
-  "ConvertJsonToPodcastSettings" should "be bale to convert JSON to PodcastSettings" in {
+  "ConvertJsonToPodcastSettings" should "be able to convert JSON to PodcastSettings" in {
     val result = ConvertJsonToPodcastSettings(testJSON.toString)
     result.destination shouldEqual defaultSettings.destination
     result.source shouldBe defaultSettings.source
     result.extensions shouldBe defaultSettings.extensions
   }
+
 }
